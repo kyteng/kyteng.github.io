1d19d271eeea74817b8fbd58aad93d4eb66db9a7
KY Teng <ky_teng88@hotmail.com>
2021-10-01 21:03:58 +0800

    2103

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,24 @@ function transformUppercase(_val) {
     return _val.toUpperCase();
 }
 
+//DOM event listen
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup", function () {
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword() {
+    let elKatalaluan = document.getElementById("kataLaluan")
+    let pass_val = elKatalaluan.value
+
+    if (pass_val.length > 5) {
+        alert("Password check...OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+
+//traditional DOM Event Handler
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword
