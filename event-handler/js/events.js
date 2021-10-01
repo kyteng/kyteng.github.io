f5322de70edbd036f4774e711ee3b0fc7d5c9f6b
KY Teng <ky_teng88@hotmail.com>
2021-10-01 20:24:14 +0800

    add js

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,7 @@ function transformUppercase(_val) {
     return _val.toUpperCase();
 }
 
-let elNamaPengguna = document.getElementById("namePengguna")
+let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup", function () {
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
